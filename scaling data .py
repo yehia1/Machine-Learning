5f70e228c2b0f91f9478ecd2d78f573a65cbba0e
yehia1 <44003515+yehia1@users.noreply.github.com>
2020-09-29 22:18:15 +0200

    Update scaling data .py

--- scaling data .py
+++ scaling data .py
@@ -14,5 +14,5 @@ x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.2,random_stat
 #scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
-x_train = sc.fit_transform(X_train)
-x_test = sc.transform(X_test)
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
