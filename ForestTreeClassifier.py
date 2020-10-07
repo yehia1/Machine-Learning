83620b715d0d6b8ce49acdd6f642d0787d743e51
yehia1 <44003515+yehia1@users.noreply.github.com>
2020-10-07 21:06:44 +0200

    Create ForestTreeClassifier.py

---
+++ ForestTreeClassifier.py
@@ -0,0 +1,47 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+file = pd.read_csv("Social_Network_Ads.csv")
+x = file.iloc[:,2:-1].values
+y = file.iloc[:,-1].values
+
+# for testing and training
+from sklearn.model_selection import train_test_split 
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.25,random_state=0 )
+
+#for scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+
+#making the logistic regression model
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=10, criterion = 'entropy')
+classifier.fit(x_train,y_train)
+y_predict = classifier.predict(x_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_predict)
+
+
+#visualising the Training set results
+from matplotlib.colors import ListedColormap
+x_set, y_set = x_train, y_train
+x1, x2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
+             alpha = 0.75, cmap = ListedColormap(('b', 'g')))
+plt.xlim(x1.min(), x1.max())
+plt.ylim(x2.min(), x2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
+                c = ListedColormap(('yellow', 'k'))(i), label = j)
+plt.title('DecisionTreeClassifier (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
